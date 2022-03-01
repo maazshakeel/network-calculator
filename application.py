7070f71833cc62e1920810be028c5ddacdc14bc8
maazshakeel <maaz.shakeel.bodla@gmail.com>
2022-03-02 00:21:56 +0500

    cleaned our code

--- application.py
+++ application.py
@@ -1,27 +1,38 @@
+# modules
 from flask import *
 import ipaddress
 
+# initialzing our app
 app = Flask(__name__, template_folder='.')
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
-    variable = False
+    # if post request
+    post_req = False
+    # get request
     if request.method == 'GET':
-        return render_template("index.html", variable=variable)
+        # rendering template index.html
+        return render_template("index.html", post_req=post_req)
     else:
+        # getting the ip
         ip = request.form.get('ip')
 
+        # getting all the details
         IP_Addr = ipaddress.ip_interface(ip)
 
+        # Details
         Net_Addr = IP_Addr.network
         pref_len = IP_Addr.with_prefixlen
         Mask = IP_Addr.with_netmask
         wildcard = IP_Addr.hostmask
         broadcast_address = Net_Addr.broadcast_address
 
+        # first octet (number/word) of ip
         first_octet = int(ip.split('.')[0])
+        # class
         class_of_octet = ''
 
+        # checking which class
         if first_octet >= 1 and first_octet <= 127:
             class_of_octet = "A"
         if first_octet >= 128 and first_octet <= 191:
@@ -29,28 +40,24 @@ def index():
         if first_octet >= 192 and first_octet <= 223:
             class_of_octet = "C"
 
+        # first ip array
         array_first_ip = str(Net_Addr).split('.')
+        # last ip array
         array_last_ip = str(broadcast_address).split('.')
 
-        last_octet = int((str(Net_Addr).split('.')[3]).split('/')[0])
-        last_octet_1 = int((str(broadcast_address).split('.')[3]).split('/')[0])
+        
+        # first ip last octet
+        first_ip_last_octet = int((str(Net_Addr).split('.')[3]).split('/')[0])
+        # last ip last octet
+        last_ip_last_octet = int((str(broadcast_address).split('.')[3]).split('/')[0])
 
 
+        # usable host
         octet_of_hostable = int((str(Net_Addr).split('.')[3]).split('/')[1])
-        total_host = (2 ** (32 - octet_of_hostable)) - 2
+        useable_host = (2 ** (32 - octet_of_hostable)) - 2
 
-        last_ip = f'{array_last_ip[0]}.{array_last_ip[1]}.{array_last_ip[2]}.{last_octet_1 - 1}'
-        first_ip = f'{array_first_ip[0]}.{array_first_ip[1]}.{array_first_ip[2]}.{last_octet + 1}'
-        return render_template("index.html", total_host=total_host, Net_Addr=str(Net_Addr).split('/')[0], pref_len=pref_len.split('/')[1], Mask=Mask.split('/')[1], wildcard=wildcard, broadcast_address=broadcast_address, variable=True, first_ip=first_ip, last_ip=last_ip, classes=class_of_octet)
+        # our last ip and first ip
+        last_ip = f'{array_last_ip[0]}.{array_last_ip[1]}.{array_last_ip[2]}.{last_ip_last_octet - 1}'
+        first_ip = f'{array_first_ip[0]}.{array_first_ip[1]}.{array_first_ip[2]}.{first_ip_last_octet + 1}'
 
-        return first_ip
-    
-        return str(Net_Addr)
-
-
-        last_octet = int((str(Net_Addr).split('.')[3]).split('/')[0])
-        last_octet_1 = int((str(broadcast_address).split('.')[3]).split('/')[0])
-        first_ip = last_octet + 1
-        last_ip = last_octet_1 - 1
-
-
+        return render_template("index.html", useable_host=useable_host, Net_Addr=str(Net_Addr).split('/')[0], pref_len=pref_len.split('/')[1], Mask=Mask.split('/')[1], wildcard=wildcard, broadcast_address=broadcast_address, post_req=True, first_ip=first_ip, last_ip=last_ip, classes=class_of_octet)
